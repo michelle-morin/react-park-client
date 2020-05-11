f83656a0f948eeee9ac0818aeec98a34d69da667
Michelle Morin <morin2m@gmail.com>
2020-05-11 11:39:20 -0700

    add compose function to include both thunk and redux dev tools
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
+import parksReducer from './reducers/parks-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  parksReducer, 
+  compose(
+    applyMiddleware(thunkMiddleware, middlewareLogger), 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
