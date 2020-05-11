d2894aed7fcbbd69e1ce271be183044313f07bca
Michelle Morin <morin2m@gmail.com>
2020-05-11 11:04:02 -0700

    create action creators and test all action creators
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

---
+++ src/__tests__/actions/index.test.js
@@ -0,0 +1,27 @@
+import * as actions from './../../actions';
+import * as c from './../../actions/ActionTypes';
+
+describe('park reducer actions', () => {
+  it('requestParks should create REQUEST_PARKS action', () => {
+    expect(actions.requestParks()).toEqual({
+      type: c.REQUEST_PARKS
+    });
+  });
+
+  it('getParksSuccess should create GET_PARKS_SUCCESS action', () => {
+    const parks = "A park";
+    expect(actions.getParksSuccess(parks)).toEqual({
+      type: c.GET_PARKS_SUCCESS,
+      parks
+    });
+  });
+
+  it('getParksFailure should create GET_PARKS_FAILURE action', () => {
+    const error = "An error";
+    expect(actions.getParksFailure(error)).toEqual({
+      type: c.GET_PARKS_FAILURE,
+      error
+    });
+  });
+
+});
---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import * as c from './ActionTypes';
+
+export const requestParks = () => ({
+  type: c.REQUEST_PARKS
+});
+
+export const getParksSuccess = (parks) => ({
+  type: c.GET_PARKS_SUCCESS,
+  parks
+});
+
+export const getParksFailure = (error) => ({
+  type: c.GET_PARKS_FAILURE,
+  error
+});
